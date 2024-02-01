6e8e8d17d8b8259d1117ebc4bcb7dcc40b59f66e
chengfangtu <1161671154@qq.com>
2024-02-01 10:07:16 +0800

    推送taskmonitor

--- TaskMonitor/taskmonitor_v2.py
+++ TaskMonitor/taskmonitor_v2.py
@@ -134,7 +134,7 @@ class Job():
             project_id = tmp[3]
             id = tmp[5] #int类型
             resplit_type = tmp[6]
-            record = tmp[7]
+            record = tmp[7] if tmp[7] == None else ""
             delta_hours = delta_time( create_time )
             start_time_f = create_time.strftime(self.time_format)
             out_value = [str(id), fc_no, project_id, start_time_f, '{0:.2f}'.format(delta_hours), str(resplit_type), record ]
