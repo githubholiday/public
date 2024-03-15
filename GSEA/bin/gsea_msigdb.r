0db36fa45ee03d21e7406d7a0d265e541537750e
chengfangtu <1161671154@qq.com>
2024-03-15 16:19:09 +0800

    推送taskmonitor

--- GSEA/bin/gsea_msigdb.r
+++ GSEA/bin/gsea_msigdb.r
@@ -1,10 +1,11 @@
 library('getopt')
 para<- matrix(c(
   'help',	'h',	0,	"logical",
-  'infile',	'i',	1,	"character",
+  'indir',	'i',	1,	"character",
+  'prefix',	'pr',	1,	"character",
   'type',	't',	2,	"character",
   'organism',	'g',	2,	"character",
-  'outdir',	'o',	1,	"character"
+  'outdir',	'o',	1,	"character",
 ),byrow=TRUE,ncol=4)
 #===========================================================
 # 仅针对人和小鼠的GO,KEGG,Reactome和MSigDB数据库，可以做enricher（传统富集方法，超几何分布，也就是Fisher exact test）和GSEA分析：
@@ -13,98 +14,98 @@ para<- matrix(c(
 # 利用ClusterProfile进行富集分析：Y叔更新快，不用担心数据库过时，操作方便，出图好看易调节。。
 opt <- getopt(para,debug=FALSE)
 print_usage <- function(para=NULL){
-  cat(getopt(para,usage=TRUE))
-  cat("infile format:
-      输入文件gene list(必须带有log2FC值,原则上rpkm或者order值也是可以运行成功的)，只接受SYMBOL/ENSEMBL/ENTREZID三种格式，
-      暂时不接受其他格式。原则上GO数据库支持20个物种，KEGG支持几乎所有物种，Reactome支持7个物种,MSigDB支持11个物种。
-      但因为org数据库包（包很大，比较占空间）只安装人和小鼠的，因此该脚本只可以做人和小鼠的基因功能富集分析，其他物种不行。
-      可以做enricher（传统富集方法，超几何分布，也就是Fisher exact test）和GSEA分析。
-      ===========================================================================
-      *Log2FC不能为NA,lnf,-lnf值
-      注意如果是lncRNA的转录组DEseq结果会有lnf,-lnf值。
-      需要自行处理+1，重新得到Log2FC值,#data$Log2FC<-log2((data[,2]+1)/(data[,3]+1))
-      ===========================================================================
-      Gene	Log2FC	Significant
-      A1BG	-4.33361161397935	yes
-      A1BG-AS1	-8.23310826049641	yes
-      A2M	1.03297056884149	no
-      A2M-AS1	0.106991842587118	no
-      A2ML1	1.0046255780959	no
-      ===========================================================================
-      type          enricher(ORA)和GSEA分析时候，输入文件是SYMBOL/ENSEMBL/ENTREZID。
-      ===========================================================================
-      outdir:outdir  of outputs,we will setwd(opt$outdir)
-      Usage example:
-      Rscript this.r -i gene_list_file -pr prefix -o outdir --pvalue 0.05 -t SYMBOL -g human/mouse -d GO,KEGG,Reactome,MSigDB
-      Options:
-      --help		h	NULL		get this help
-      --infile		i	character	input gene file [forced]
-      --outdir		o	character	The	resurt of out dir for analysis [forced]
-      --type		t	character	one of SYMBOL/ENSEMBL/ENTREZID [forced]
-      --organismg	g	character	human or mouse
-      \n")
-  q(status=1)
+    cat(getopt(para,usage=TRUE))
+    cat("
+【功能说明】
+    该脚本进行人和小鼠的Msigdb的GSEA分析，只输出GSEA表格和rds结果，不进行绘图。如果坐其他物种，需要下载其他物种的org数据库包。
+【参数说明】
+    infile: 输入文件，第一列必须为gene,必须包含Log2FC列。说明:gene列的必须是SYMBOL/ENSEMBL/ENTREZID三种格式。Log2FC不能为NA,lnf,-lnf值
+    prefix:输出文件前缀
+    type:输入文件中gene的类型，可选 SYMBOL/ENSEMBL/ENTREZID。
+    outdir:输出目录
+    prefix:输出结果前缀
+【使用示例】
+    Usage example:
+    Rscript gsea_msigdb.r -i gene_list_file -pr prefix -o outdir -t SYMBOL -g human/mouse
+    Options:
+    --help		h	NULL		get this help
+    --infile		i	character	input gene file [forced]
+    --outdir		o	character	The	resurt of out dir for analysis [forced]
+    --prefix		pr	character	prefix of output [forced]
+    --type		t	character	one of SYMBOL/ENSEMBL/ENTREZID [forced]
+    --organismg	g	character	human or mouse
+    \n")
+    q(status=1)
 }
 #===========================================================
 if ( !is.null(opt$help) )	{ print_usage(para) }
-if ( is.null(opt$infile) )	{ cat("Please input the data file1 ...\n\n") ; print_usage(para)}
+if ( is.null(opt$indir) )	{ cat("Please input the data file1 ...\n\n") ; print_usage(para)}
 if ( is.null(opt$outdir) )	{ cat("Please give the outdir for analysis ...\n\n") ; print_usage(para) }
+if ( is.null(opt$prefix) )	{ cat("Please give the prefix for outputfiles ...\n\n") ; print_usage(para) }
 if ( is.null(opt$organism) )	{ cat("Please give the organism(human/mouse) ...\n\n") ; print_usage(para) }
 if ( is.null(opt$type) )	{ cat("Please give the type of gene(SYMBOL/ENSEMBL/ENTREZID) ...\n\n") ; print_usage(para) }
-if ( is.null(opt$database))	{ opt$database <- 'GO,KEGG,reactome'} 
+if ( is.null(opt$pvalue))	{ opt$pvalue <- 0.05} 
 #===========================================================
 library(clusterProfiler)
-library(org.Hs.eg.db)
-library(org.Mm.eg.db)
 library(ReactomePA)
 library(msigdbr)
 library(DOSE)
-library(enrichplot)
-library(ggplot2)
 library(ggupset)
-library(cowplot)
-#===========================================================
-mkdirs <- function(outdir) {
-  if(!file.exists(outdir)) {
-    #mkdirs(dirname(fp))
-    dir.create(outdir)}
-  else{print(paste(outdir,"Dir already exists!",sep="     "))
-    unlink(outdir, recursive=TRUE)
-    dir.create(utdir,fp)
-    }
+
+infile<-opt$infile
+outdir<-opt$outdir
+prefix<-opt$prefix
+type<-opt$type #数据类型
+organism<-opt$organism #物种，human或者mouse
+#创建输出目录
+mkdirs(opt$outdir)
+
+if(organism=='human'){
+    mapda<-org.Hs.eg.db
+    library(org.Hs.eg.db)
+}else{
+    mapda<-org.Mm.eg.db
+    library(org.Mm.eg.db)
 }
 
-#===========================================================
-de_genes<-function(infile,species,type, outfile ){
-    # infile 是基因列表文件，第一列为gene名称，需包含Log2FC列
-    # species 人和小鼠的org.Hs.eg.db和org.Mm.eg.db
-    # type  SYMBOL/ENSEMBL/ENTREZID
-    # outfile 输出文件
-    data<-read.table(infile,header=T,sep='\t')
-    colnames(data)[1] <- type
-    k=keys(species,keytype = type)
-    geneinfo <- bitr(k,fromType = type,toType = c("ENTREZID"),OrgDb = species)
-    geneset<-merge(geneinfo,data,by=type)
-    #分别取差异基因构建geneList(GSEA)和gene(ORA)变量
-    print(paste('enrich gene number:',length(gene)))
-    #获取基因ID和log2FC列信息
-    geneList <- geneset$Log2FC
-    names(geneList) <- as.character(geneset$ENTREZID)
-    #对其进行排序
-    geneList <- sort(geneList, decreasing = TRUE)
-    write.table(geneList, outfile,quote=F,sep='\t',row.names=F)
-    return(geneList)
+#1.生成geneList(for GSEA)变量
+print("Start: Msigdb database with GSEA analysis")
+geneList<-de_genes(infile,mapda,type)
+mkdirs(outdir)
+    #只能做人和小鼠的，如果不是人的，默认就是小鼠的，实际上能做的类型很多，目前不兼容
+if(organism=='human'){
+    species<-'Homo sapiens'
+}else{
+    species<-'"Mus musculus'
 }
+
+xyMSigDB<-MSigDB_clusterProfiler( geneList, species, paste(outdir,"gse_result.rds", sep="_" ))
+#将gene id转化为gene_name
+y<- as.data.frame(setReadable(xyMSigDB$gse, OrgDb = mapda,keyType = "ENTREZID"))
+
+terms<-xyMSigDB$Terms[,c('gs_cat','gs_id')]
+colnames(terms)<-c('Category','ID')
+n2<-dim(y)[2]
+y<-merge(y, terms,by='ID')
+colN<-c('Category',colnames(y)[1:n2])
+y<-y[,colN]
+y<-y[order(y$p.adjust),]
+#为了保持sort排序保持一致，MSigDB在GSEA_plot里write，因存在大量p.adjust相同值的terms
+write.table(y,file=paste(outdir,"gsea_result.xls",sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
+saveRDS(xyMSigDB$gse, file=paste(outdir,"gsea_result.rds",sep="_"))
+
+print("Finish: Msigdb database with GSEA analysis")
 
 #===========================================================
 #最多可支持"Homo sapiens,Mus musculus等11个物种，可通过msigdbr_show_species()查看。
-MSigDB_clusterProfiler<-function(geneList, spe){
+MSigDB_clusterProfiler<-function(geneList, spe, rds_file){
     m_t2g <- msigdbr(species = spe) %>% dplyr::select(gs_cat,gs_id,gs_name, entrez_gene)
     term_genes<-m_t2g[,c('gs_id','entrez_gene')]
     terms<-unique(m_t2g[,c('gs_cat','gs_id','gs_name')])
     term_names<-terms[,c('gs_id','gs_name')]
     y<-GSEA(geneList, TERM2GENE = term_genes, TERM2NAME = term_names,nPerm = 10000,pvalueCutoff =1,
     minGSSize = 1, maxGSSize = 50000)
+    saveRDS(y, file = rds_file)
     xy<-list( gse=y,Terms=terms)
     return(xy)
 }
@@ -114,50 +115,39 @@ setReadable_write<-function(y,outfile){
     y<- setReadable(y, OrgDb = mapda,keyType = "ENTREZID")
     write.table(y,file=outfile,sep="\t", quote=FALSE, row.names=FALSE)
 }
-
-#输入数据处理
-infile <- opt$infile
-outdir <- opt$outdir
-organism<-opt$organism
-type <- opt$type
-
-mapda<-switch(organism,
-    "human"=org.Hs.eg.db,
-    "mouse"=org.Mm.eg.db,
-    stop("Please input the correct organism(human/mouse)!\n"))
-
-outfile <- paste(outdir,"de_genes.txt",sep="/")
-de_genes(infile,mapda,type, outfile )
-
-opt$database <- "MSigDB"
-databaseList <- strsplit(opt$database, ",")[[1]]
-#5-1.MSigDB数据库(分别做ORA和GSEA test)
-if ("MSigDB" %in% databaseList){
-    print("Start:MSigDB analysis...")
-    sign = "MSigDB"
-    result_dir = paste(outdir,sign,sep="/")
-    mkdirs(result_dir)
-
-    #只能做人和小鼠的，如果不是人的，默认就是小鼠的，实际上能做的类型很多，目前不兼容
-    if(organism=='human'){
-        species<-'Homo sapiens'
+#===========================================================
+mkdirs <- function(outdir) {
+    if(!file.exists(outdir)) {
+        dir.create(outdir)
     }else{
-        species<-'"Mus musculus'
-        }
-    
-    xyMSigDB<-MSigDB_clusterProfiler(geneList, species)
-    y<- as.data.frame(setReadable(xyMSigDB$gse, OrgDb = mapda,keyType = "ENTREZID"))
-
-    terms<-xyMSigDB$Terms[,c('gs_cat','gs_id')]
-    colnames(terms)<-c('Category','ID')
-    ##################################
-    n2<-dim(y)[2]
-    y<-merge(y, terms,by='ID')
-    colN<-c('Category',colnames(y)[1:n2])
-    y<-y[,colN]
-    y<-y[order(y$p.adjust),]
-    #为了保持sort排序保持一致，MSigDB在GSEA_plot里write，因存在大量p.adjust相同值的terms
-    write.table(y,file=paste(result_dir,"gse_result.xls",sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
-    saveRDS(xyMSigDB$gse, file=paste(result_dir,"gse_result.rds",sep="_"))
-    print("Finish:MSigDB database with enricher(ORA) and GSEA test!")
+        print(paste(outdir,"Dir already exists!",sep="     "))
+        unlink(outdir, recursive=TRUE)
+        dir.create(outdir)
+    }
+}
+#===========================================================
+de_genes<-function(indir,species,type){
+    #人和小鼠转化使用的物种信息
+    #mapda<-org.Hs.eg.db
+    #mapda<-org.Mm.eg.db
+    data<-read.table(indir,header=T,sep='\t')
+    ######################
+    #构建新Log2FC, 避免lnf,-lnf值
+    #data$Log2FC<-log2((data[,2]+1)/(data[,3]+1))
+    colnames(data)[1] <- type
+    k=keys(species,keytype = type)
+    #SYMBOL/ENSEMBL/ENTREZID
+    geneinfo <- bitr(k,fromType = type,toType = c("ENTREZID"),OrgDb = species)
+    geneset<-merge(geneinfo,data,by=type)
+    #分别取差异基因构建geneList(GSEA)和gene(ORA)变量
+    gene<-unique(as.vector(subset(geneset)$ENTREZID))
+    #gene<-unique(as.vector(subset(geneset,Significant=='yes')$ENTREZID))
+    print('enrich gene number:')
+    print(length(gene))
+    d<-geneset
+    geneList <- d$Log2FC
+    #names(geneList) <- as.character(d$ENTREZID)
+    #geneList <- sort(geneList, decreasing = TRUE)
+    #genes<- list(gene=gene, geneList=geneList,bg=as.vector(geneinfo$ENTREZID))
+    return(geneList)
 }
--- GSEA/bin/gsea_plot.r
+++ GSEA/bin/gsea_plot.r
@@ -2,20 +2,28 @@
 library('getopt')
 para<- matrix(c(
     'help', 'h',    0,  'logical',
-    'infile',   'i',    1,  'character',
+    'rds',  'r',      1, 'character',
+    'idfile',   'i',    1,  'character',
     'outdir',   'o',    1,  'character',
-    'prefix',   'p',    1,  'character'
+    'prefix',   'p',    1,  'character',
+    'header',   'header', 1,  'logical'
 ),byrow=TRUE, ncol=4)
 
 opt <- getopt(para, debug=FALSE)
 print_usage <- function(para=NULL) {
     cat(getopt(para, ueasge=TRUE))
-    cat("参数说明：
-    --infile,-i,[required]输入文件
+    cat("
+【功能说明】
+    对GSEA的结果进行经典图绘制,输入为一列ID即可。图中展示NES,pvalue, p.adjust三列信息
+【参数说明】
+    --rds,-r,[required]输入文件,gsea结果的rds文件
+    --idfile,-i,[required]需要绘图的ID列表输入文件（可以包含表头，也可以不包含）
     --outdir,-o,[required]输出目录
     --prefix,-p,[required]输出文件前缀
-    示例：
-    Rscript gsea_plot.r -i infile -o outdir -p test
+    --header,-header,[optional]是否包含标题,默认不包含
+    --help,-h,[optional]帮助文档
+【使用示例】
+    Rscript gsea_plot.r -r gsea.rds -i infile -o outdir -p test -header False
     ")
     q(status=1)
 }
@@ -33,18 +41,44 @@ prefix <- para$prefix
 
 library(enrichplot)
 library(ggplot2)
+library(ggpp)
 
 
 ## 
-data <- read.table( infile, header=T, sep='\t')
-for (i in (1:nrow(data))){
-    tmp = data[i,]
-    id = tmp$ID
-    score = tmp$enrichmentScore
-    title = paste( id,"_",score,sep="")
-    pdf_file = paste(outdir,"/", prefix, title,".gsea.pdf"sep="")
-    pdf(pdf_file)
-    p1<-gseaplot2(tmp, geneSetID =id,pvalue_table=T,title= title)
-    print(p1)
+rds_data <- readRDS(rds_data)
+ids_data <- read.table( infile, header=T, sep='\t')
+outpre <- paste( outdir, prefix, sep='/')
+GSEA_plot( rds_data, outpre, ids )
+
+GSEA_plot<-function(gsea_res,out_pre, ids ){
+    library(ggpp)
+    #如果想将所有id的画在一个图中，不循环即可
+    for (k in (1:length(ids))){
+        geneSetID = ids[k]
+        filename<-paste(out_pre, geneSetID,'GSEA_score.pdf',sep='_')
+        pdf(filename, w=12, h=8)
+        
+        pd <- gsea_res[geneSetID, c( "NES","pvalue", "p.adjust")]
+        rownames(pd) <- NULL
+        for (i in seq_len(ncol(pd))){pd[, i] <- format(pd[, i], digits = 4)}
+        pd_t <- data.frame(t(pd))
+        data<-cbind("Type" = rownames(pd_t),pd_t)
+        colnames(data) <- c("Type","Value")
+        pd <- data
+        
+        #绘图
+        p <- gseaplot2(gsea_res, geneSetID = geneSetID)
+
+        p[[1]] <- p[[1]]+
+            annotate("table", x = 12000, y = 0.4, label = pd,
+            size=3,table.theme = ttheme_gtminimal
+            )+
+            theme(plot.title = element_text(size = 5),
+                legend.position = "top",
+                legend.direction = "vertical"
+            )
+        print(p)
+        dev.off()
+        }
 }
 
