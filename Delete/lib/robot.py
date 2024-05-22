f2f59c6e915603937428ad0787b2579185c6c65d
tuchengfang <1161671154@qq.com>
2024-05-22 10:22:35 +0800

    推送taskmonitor

--- Delete/lib/robot.py
+++ Delete/lib/robot.py
@@ -22,7 +22,7 @@ __doc__ = """
 #real_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=90e20ec2-1f14-434d-83ad-56a0d40e6830"
 ##纯测序与微生物项目部群
 #real_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a5039603-857e-472f-99d0-7022f0a57f10"
-real_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=baff4900-5da6-4c6e-89eb-ae4b8841d1bd"
+real_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a5039603-857e-472f-99d0-7022f0a57f10"
 class Robots():
 
     def __init__(self,title,label_list,content_list):                                                              #调用该类时，需要传入这3个参数，以下为调用该函数时就会发生的，生成属性和执行方法，而不是用main函数来执行
@@ -36,23 +36,23 @@ class Robots():
         self.send_txt(title=self.title,label=self.label,mes=self.mes,url=real_url)
     
     def content_list_to_markdown(self,rets):
-        mes=' --- '
+        mes=''
         if len(rets) == 0:                                                                                      #若果输入的rets也就时列表是空的，mes不变还是---
-            mes = '  --- '
+            mes = '\t'
         else:
             for ret in rets:
                 for i in ret:
-                    mes += str(i) + ' --- '                                                                     #否则就是把rets列表里的内容用---连接起来
-                mes += '\n|\n'                                                           #多的这个'\n|\n'，是因为可能需要Markdown的内容有很多，也就时有多条项目信息要统计，需要换行
+                    mes += str(i) + ' | '                                                                     #否则就是把rets列表里的内容用---连接起来
+                mes += '\n'                                                           #多的这个'\n|\n'，是因为可能需要Markdown的内容有很多，也就时有多条项目信息要统计，需要换行
         return mes
     
     def label_list_to_markdown(self,rets):
-        mes=' --- '
+        mes=''
         if len(rets) == 0:
-            mes = ' --- '
+            mes = '\t'
         else:
             for ret in rets:
-                mes += str(ret) + ' --- '
+                mes += str(ret) + ' | '
         return mes
 
         
@@ -62,7 +62,7 @@ class Robots():
         headers = {"Content-Type": "text/plain"}                                            #字典
         send_url = url
         #"""
-        if mes == "|":
+        if mes == "\t":
             send_data={
                 "msgtype": "markdown", #消息类型，固定为markdown
                 "markdown":{
@@ -75,7 +75,7 @@ class Robots():
                 "msgtype": "markdown", # 消息类型，固定为markdown
                 "markdown":{
                     "content":"# <font color=\"warning\">Everyone Attention!!!!!</font>\n" +
-                    "# <font color=\"warning\">Attention! {0}~ </font>\n".format(title) + "## **{0}**\n|\n{1}".format(label,mes) +
+                    "# <font color=\"warning\">Attention! {0}~ </font>\n".format(title) + "## **{0}**\n{1}".format(label,mes) +
                     "> <font color=\"warning\"> **请注意\n@所有人**</font>"
                 }
             }
