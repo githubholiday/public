c747c6a46057046c07708efd8eddec53577026f9
chengfangtu <1161671154@qq.com>
2024-05-27 13:47:35 +0800

    推送taskmonitor

--- t7_atac/bin/atac.mk
+++ t7_atac/bin/atac.mk
@@ -13,12 +13,13 @@ Help:
 	@echo -e "\t" 对T7下机的ATAC数据进行截取和处理，并进行gzip压缩
 	@echo Usage:
 	@echo -e "\n" Usage获取Config文件
-	@echo -e "\t" make -f ${file} project_id= hcsv= outfile= I5_discard= R2_discard=GetSampleList
+	@echo -e "\t" make -f ${file} project_id= hcsv= outfile= I5_discard= R2_discard= raw_fq= GetSampleList
 	@echo  Parameters:
 	@echo -e "\t" project_id: 子项目编号
 	@echo -e "\t" hcsv: 拆分目录下的Hcsv文件
 	@echo -e "\t" I5_discard: 获取I5需要截取掉的长度，使用参数为seqkit -b
 	@echo -e "\t" R2_discard: 获取R2需要截取掉的长度，使用参数为seqkit -e
+	@echo -e "\t" raw_fq: 原始数据路径，给到样本上一层，如拆分的fastq/Project_PM1 
 	@echo -e "\t" outfile: 生成的config.ini文件
 	@echo -e "\n" Usage:获取ATAC的I5和R2数据长度
 	@echo -e "\t" make -f ${file} R2= discard_len= outfile= ATAC_I5
@@ -32,8 +33,10 @@ outdir=$(dir $(abspath $(firstword $(outfile))))
 GetSampleList:
 	echo "########## GetSampleList start at" `date`
 	mkdir -p $(outdir)/
-	echo "[Sample]\n" >  $(outfile)
+	echo "[Sample]" >  $(outfile)
 	grep $(project_id) $(hcsv) |awk -F "," '{print $$3"\t"$$11"\t"$(I5_discard)"\t"$(R2_discard)}' >> $(outfile)
+	echo "[Para]" >> $(outfile)
+	echo "Para_raw_fq=$(raw_fq)" >> $(outfile)
 	echo "########## GetSampleList  finish at" `date`
 
 outdir=$(dir $(abspath $(firstword $(outfile))))
