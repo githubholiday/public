dd696fa8f9d0676e037fb77eb38f79ee21fe705e
tuchengfang <1161671154@qq.com>
2024-10-28 09:58:44 +0800

    推送taskmonitor

--- scrna_module/Basic_Analysis/bin/script/a_sample_findallmakers.r
+++ scrna_module/Basic_Analysis/bin/script/a_sample_findallmakers.r
@@ -26,30 +26,36 @@ print_usage <- function(para=NULL) {
 
 #对输入进行检查
 if ( !is.null(opt$help) )   { print_usage(para) }
-if ( is.null(opt$infile) )  { cat("Please input the data file\n\n") ; print_usage(para)}
+if ( is.null(opt$rds) )  { cat("Please input the data file\n\n") ; print_usage(para)}
 if ( is.null(opt$outdir) )  { cat("Please input the outdir of result\n\n") ; print_usage(para)}
 if ( is.null(opt$prefix) )  { cat("Please input the prefix of result\n\n") ; print_usage(para)}
 
   
 #对输入进行赋值处理
-infile <- para$infile
-outdir <- para$outdir
-prefix <- para$prefix
+infile <- opt$rds
+outdir <- opt$outdir
+prefix <- opt$prefix
  
 library(Seurat)
 library(dplyr)
 
 rds <- readRDS(opt$rds)
-Idents(rds) <- args$Ident
+Idents(rds) <- opt$Ident
 all.markers <- FindAllMarkers( rds,logfc.threshold = 0.1, test.use = "wilcox",min.pct = 0.01 )
 #增加基因列
 all.markers <- cbind("Gene"=rownames(all.markers),all.markers)
 
 #判断上下调和是否显著
 all.markers <- all.markers %>% mutate(up_down = ifelse(avg_log2FC > 0, "Up", "Down"),significant = ifelse(p_val_adj < 0.05, "yes", "no"))
-out_pre <- paste(opt$outdir, $prefix, sep="/")
 
-for (i in unique(sto@meta.data$seurat_clusters)){
+
+out_pre <- paste(outdir, prefix, sep="/")
+
+all_marker_out <- paste( out_pre, ".all.marker.xls", sep="")
+write.table(all.markers,"all.marker.xls", sep="\t", quote=FALSE,row.names = FALSE)
+
+
+for (i in unique(Idents(rds))){
 	markers <- all.markers[ all.markers$cluster==i,]
 	outfile <- paste(out_pre, i, "marker.xls",sep="_")
 	write.table(markers, outfile, sep="\t", quote=FALSE,row.names = FALSE )
