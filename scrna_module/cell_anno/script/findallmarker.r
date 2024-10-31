f6381dab024f75c94e2752c7d7c7abf82dafbe76
chengfangtu <1161671154@qq.com>
2024-10-31 17:00:08 +0800

    推送taskmonitor

--- scrna_module/cell_anno/script/findallmarker.r
+++ scrna_module/cell_anno/script/findallmarker.r
@@ -10,8 +10,7 @@ command=matrix(c(
   'test.use', 't', 2, 'character', '检验方法，默认为wilcox，可选为wilcox或roc等',
   'Ident' , "I" , 2 , 'character' , '聚类的标签，默认为harmony_clusters， 可选为任意列名'
 ),
-  byrow=T,ncol=5
-)
+  byrow=T,ncol=5)
 
 ## 读取参数
 args=getopt(command)
@@ -58,7 +57,7 @@ if ( is.null(args$test.use)) {
 
 
 print(paste0("test.use:",test.use))
-
+library(dplyr)
 
 if ( is.null(args$Ident)) {
     Ident.col <- "harmony_clusters"
@@ -82,38 +81,29 @@ pbmc <- readRDS(args$input)
 print(paste0("检查meta信息中是否有",Ident.col,"信息"))
 if ( !Ident.col %in% colnames(pbmc@meta.data)){
   cat("Error: meta信息中没有",Ident.col,"信息!, 请检查\n")
-  print(colnames(pbmc@meta.data))
   q()
 }else{
   Idents(pbmc) = Ident.col
 }
-#按照每个cluster输出marker
 
-
-all_cluster <- sort(unique( unlist(pbmc[[Ident.col]]) ))
-for (i in all_cluster){
-    outfile <- paste0(args$outdir,"/",args$name,".cluster",i,".markers.xls")
-    cluster_data <- all.markers %>% filter( all.markers[,"cluster"] %in% i)
-    write.table(cluster_data,file=outfile,sep = "\t",quote = FALSE,row.names = FALSE)
-}
- 
 
 all.markers <- FindAllMarkers(object = pbmc , only.pos = TRUE, min.pct = min.pct, logfc.threshold = logfc.threshold, test.use = test.use)
-all.markers <- all.markers[,c(7,6,1,2,3,4,5)]
+#all.markers <- all.markers[,c(7,6,1,2,3,4,5)]
+
+#增加基因列
+all.markers <- cbind("Gene"=all.markers[,"gene"],all.markers)
+#判断上下调以及是否显著
+all.markers <- all.markers %>% mutate(up_down = ifelse(avg_log2FC > 0, "Up", "Down"),significant = ifelse(p_val_adj < 0.05, ifelse(abs(avg_log2FC) > 0.25, "yes", "no"), "no"))
+#输出总的all_marker表
 write.table(all.markers,file = paste0(args$outdir,"/",args$name,".markers.xls") ,sep = "\t",quote = FALSE,row.names = FALSE)
 
 write.table(pbmc$harmony_clusters,file = paste0(args$outdir,"/",args$name,".cluster.xls") ,sep = "\t",quote = FALSE ,col.names = FALSE)
 
-
+#按照每个cluster输出marker
+all_cluster <- sort(unique( unlist(pbmc[[Ident.col]]) ))
+for (i in all_cluster){
+	outfile <- paste0(args$outdir,"/",args$name,".cluster",i,".markers.xls")
+	cluster_data <- all.markers %>% filter( all.markers[,"cluster"] %in% i)
+	write.table(cluster_data,file=outfile,sep = "\t",quote = FALSE,row.names = FALSE)
+}
 print("完成")
-
-#result <-read.table("/work/share/acuhtwkcu9/Project/scRNA/P2024042410ZFSUFQ_horse/analysis/outdir/normal/cluster_0_3/annotation/singler/celltypst/predicted_labels.csv",header=T , row.names=1,sep=",")
-
-
-
-
-
-
-
-
-
